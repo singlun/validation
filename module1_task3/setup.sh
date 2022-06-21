084f2f8dc5d5a0ad6590ffb7838a11dc2a650d36
Sing Lun Tang <alantang@Sings-iMac.local>
2022-06-22 05:20:56 +0800

    first init

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,1 +1,5 @@
-docker build .
+docker run --name goHugo --rm -d --tty --interactive --workdir=/app ubuntu:18.04
+docker cp . goHugo:/app
+docker exec goHugo apt-get update 
+docker exec goHugo apt-get install -y hugo make 
+docker exec -it goHugo /bin/bash
